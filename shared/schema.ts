b798cc4a9014f87a97cb5774d6ea05546f5d2b37
Leonardo Duarte <leocc_etraud@outlook.com>
2026-02-13 21:01:02 -0300

    feat: limite de caracteres nos campos de login

--- shared/schema.ts
+++ shared/schema.ts
@@ -31,8 +31,8 @@ export const POST_TYPES = {
 
 // Zod Schemas
 export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
+  email: z.string().email().max(100),
+  password: z.string().min(6).max(50),
 });
 
 export type LoginRequest = z.infer<typeof loginSchema>;
